8dd8de30814b9300e260181390493ae022440f08
Preston Lo <prestonloz97@gmail.com>
2023-08-24 16:42:18 +0000

    change to load from JSON file

--- dist/extension.js
+++ dist/extension.js
@@ -1,1 +1,198 @@
-(()=>{"use strict";var e={496:e=>{e.exports=require("vscode")}},t={};function o(i){var n=t[i];if(void 0!==n)return n.exports;var c=t[i]={exports:{}};return e[i](c,c.exports,o),c.exports}var i={};(()=>{var e=i;Object.defineProperty(e,"__esModule",{value:!0}),e.deactivate=e.activate=void 0;const t=o(496);e.activate=function(e){let o=t.commands.registerCommand("wordrecolor.activate",(()=>{}));e.subscriptions.push(o);const i=t.window.createTextEditorDecorationType({color:"#ff00f2",fontStyle:"italic"}),n=t.window.createTextEditorDecorationType({color:"#23ff0a"}),c=t.window.createTextEditorDecorationType({color:"red"});let r;function s(){const e=t.window.activeTextEditor;if(e){const o=e.document,r=o.getText(),s=[],a=[],d=[];let u;const p=/\b(NULL|TRUE|FALSE|NONE|NOT IN)\b/gi;for(;u=p.exec(r);){const e=o.positionAt(u.index),i=o.positionAt(u.index+u[0].length),n=new t.Range(e,i);switch(u[1].toUpperCase()){case"NULL":case"NONE":s.push(n);break;case"TRUE":a.push(n);break;case"FALSE":case"NOT IN":d.push(n)}}e.setDecorations(i,s),e.setDecorations(n,a),e.setDecorations(c,d)}}function a(){r&&(clearTimeout(r),r=void 0),r=setTimeout(s,500)}t.window.activeTextEditor&&a(),t.window.onDidChangeActiveTextEditor((e=>{e&&a()}),null,e.subscriptions),t.workspace.onDidChangeTextDocument((e=>{t.window.activeTextEditor&&e.document===t.window.activeTextEditor.document&&a()}),null,e.subscriptions)},e.deactivate=function(){}})(),module.exports=i})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ }),
+/* 2 */
+/***/ ((module) => {
+
+module.exports = require("fs");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __webpack_require__(1);
+const fs = __webpack_require__(2);
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+function activate(context) {
+    // This will register command name and trigger a function with it
+    // The function is empty, because I dont need it
+    // I have to write this, else, the extension wont run
+    let disposable = vscode.commands.registerCommand('wordrecolor.activate', () => {
+        let editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // display the JSON file path that this extension will be searching for
+            // display the current active editor
+            let language = getCurrentActiveEditorLanguage();
+            vscode.window.showInformationMessage('Language: ' + language);
+            let JSONPath = getJSONPath(language);
+            vscode.window.showInformationMessage('JSON Path: ' + JSONPath);
+        }
+        else {
+            vscode.window.showInformationMessage('No language detected');
+        }
+    });
+    // This will put the command specified in package.json into command palette (CTRL + SHIFT + P)
+    context.subscriptions.push(disposable);
+    let language = getCurrentActiveEditorLanguage();
+    // for now, just make it to default.json first
+    let JSONPath = getJSONPath(null);
+    let data = getJSONData(JSONPath);
+    // just a note, you can set the italic font style and such
+    // const nullDecorationType = vscode.window.createTextEditorDecorationType({
+    //     color: '#ff00f2', // Pink
+    // 	fontStyle: 'italic',
+    // 	// This is how you do it
+    // 	// fontWeight: 'bold',
+    // 	// fontStyle: 'italic',
+    // 	// textDecoration: 'underline'
+    // });
+    // This is to delay the trigger update
+    // This is to increase performance
+    // Now, everytime you type in file, it will trigger the extension to colorize words
+    // However, we dont want it to colorize immediately, but after a short delay
+    let timeout = undefined;
+    // main function, will recolor the words
+    function updateDecorations() {
+        var editor = vscode.window.activeTextEditor;
+        if (editor) {
+            var document = editor.document;
+            var text = document.getText();
+            var ranges = [];
+            let colors = Object.keys(data);
+            for (let color of colors) {
+                console.log(color);
+                var DecorationType = vscode.window.createTextEditorDecorationType({
+                    color: color
+                });
+                let keywords = data[color];
+                for (let keyword of keywords) {
+                    console.log(keyword);
+                    let match;
+                    // a dynamic regex
+                    // match case-insensitive-ly
+                    let regex = new RegExp("\\b(" + keyword + ")\\b", "gi");
+                    while (match = regex.exec(text)) {
+                        const start = document.positionAt(match.index);
+                        const end = document.positionAt(match.index + match[0].length);
+                        const range = new vscode.Range(start, end);
+                        // put these words into array
+                        ranges.push(range);
+                    }
+                    // change the color, according to the words in the array
+                    editor.setDecorations(DecorationType, ranges);
+                }
+            }
+        }
+    }
+    function triggerUpdateDecorations() {
+        if (timeout) {
+            clearTimeout(timeout);
+            timeout = undefined;
+        }
+        timeout = setTimeout(updateDecorations, 500);
+    }
+    // This one I think apply changes based on current active file you're editting
+    if (vscode.window.activeTextEditor) {
+        triggerUpdateDecorations();
+    }
+    // This one will handle event handling
+    // This will trigger function to colotizes the word
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        if (editor) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+    // This one will handle event handling
+    // This will trigger function to colotizes the word
+    vscode.workspace.onDidChangeTextDocument(event => {
+        if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+}
+exports.activate = activate;
+// Get the JSON File
+function getJSONData(JSONPath) {
+    let fileContents = fs.readFileSync(JSONPath, "utf8");
+    let data = JSON.parse(fileContents);
+    return data;
+}
+// To get the JSON file path that the extension will be looking
+// This JSON describe what keyword to recolor
+function getJSONPath(language) {
+    // if language passed in is null, then set it to 'default.json'
+    if (language == null)
+        language = 'default';
+    return vscode.env.appRoot + '/word-recolor/' + language + '.json';
+}
+// To get the current active editor language
+function getCurrentActiveEditorLanguage() {
+    let language;
+    let editor = vscode.window.activeTextEditor;
+    if (editor) {
+        language = editor.document.languageId;
+    }
+    else {
+        language = 'No language detected';
+    }
+    return language;
+}
+// Generated by Bing
+function getFunctionNameAndLineNumber() {
+    const error = new Error();
+    const stack = error.stack?.split('\n')[2];
+    const functionName = stack?.match(/at (\S+)/)?.[1] ?? '';
+    const lineNumber = parseInt(stack?.match(/:(\d+):/)?.[1] ?? '', 10);
+    return [functionName, lineNumber];
+}
+// This method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,66 +10,79 @@ export function activate(context: vscode.ExtensionContext) {
 	// The function is empty, because I dont need it
 	// I have to write this, else, the extension wont run
 	let disposable = vscode.commands.registerCommand('wordrecolor.activate', () => {
-		;
+		let editor = vscode.window.activeTextEditor;
+		if (editor) {
+			// display the JSON file path that this extension will be searching for
+			// display the current active editor
+			let language = getCurrentActiveEditorLanguage();
+			vscode.window.showInformationMessage('Language: ' + language);
+			let JSONPath = getJSONPath(language);
+			vscode.window.showInformationMessage('JSON Path: ' + JSONPath);
+		}
+		else
+		{
+			vscode.window.showInformationMessage('No language detected');
+		}
 	});
-
+	
 	// This will put the command specified in package.json into command palette (CTRL + SHIFT + P)
 	context.subscriptions.push(disposable);
+	
+	let language = getCurrentActiveEditorLanguage();
+	// for now, just make it to default.json first
+	let JSONPath = getJSONPath(null);
+	let data = getJSONData(JSONPath);
 
-    const nullDecorationType = vscode.window.createTextEditorDecorationType({
-        color: '#ff00f2', // Pink
-		fontStyle: 'italic',
-		// This is how you do it
-		// fontWeight: 'bold',
-		// fontStyle: 'italic',
-		// textDecoration: 'underline'
-    });
-    const trueDecorationType = vscode.window.createTextEditorDecorationType({
-        color: '#23ff0a' // Green
-    });
-    const falseDecorationType = vscode.window.createTextEditorDecorationType({
-        color: 'red'
-    });
-
+	// just a note, you can set the italic font style and such
+    // const nullDecorationType = vscode.window.createTextEditorDecorationType({
+    //     color: '#ff00f2', // Pink
+	// 	fontStyle: 'italic',
+	// 	// This is how you do it
+	// 	// fontWeight: 'bold',
+	// 	// fontStyle: 'italic',
+	// 	// textDecoration: 'underline'
+    // });
+	
 	// This is to delay the trigger update
 	// This is to increase performance
 	// Now, everytime you type in file, it will trigger the extension to colorize words
 	// However, we dont want it to colorize immediately, but after a short delay
     let timeout: NodeJS.Timer | undefined = undefined;
+	
+	// main function, will recolor the words
+	function updateDecorations() {
+		var editor = vscode.window.activeTextEditor;
+		if (editor) {
+			var document = editor.document;
+			var text = document.getText();
+			var ranges: vscode.Range[] = [];
+			let colors = Object.keys(data);
+			for (let color of colors) {
+				console.log(color);
+				var DecorationType = vscode.window.createTextEditorDecorationType({
+					color: color
+				});
+				let keywords = data[color];
+				for (let keyword of keywords) {
+					console.log(keyword);
+					let match;
+					// a dynamic regex
+					// match case-insensitive-ly
+					let regex = new RegExp("\\b(" + keyword + ")\\b", "gi");
+					while (match = regex.exec(text)) {
+						const start = document.positionAt(match.index);
+						const end = document.positionAt(match.index + match[0].length);
+						const range = new vscode.Range(start, end);
+						// put these words into array
+						ranges.push(range);
+					}
+					// change the color, according to the words in the array
+					editor.setDecorations(DecorationType, ranges);
+				}
+			}
 
-    function updateDecorations() {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const document = editor.document;
-            const text = document.getText();
-            const nullRanges: vscode.Range[] = [];
-            const trueRanges: vscode.Range[] = [];
-            const falseRanges: vscode.Range[] = [];
-            let match;
-            const regex = /\b(NULL|TRUE|FALSE|NONE|NOT IN)\b/gi;
-            while (match = regex.exec(text)) {
-                const start = document.positionAt(match.index);
-                const end = document.positionAt(match.index + match[0].length);
-                const range = new vscode.Range(start, end);
-                switch (match[1].toUpperCase()) {
-                    case 'NULL':
-                    case 'NONE':
-                        nullRanges.push(range);
-                        break;
-                    case 'TRUE':
-                        trueRanges.push(range);
-                        break;
-                    case 'FALSE':
-                    case 'NOT IN':
-                        falseRanges.push(range);
-                        break;
-                }
-            }
-            editor.setDecorations(nullDecorationType, nullRanges);
-            editor.setDecorations(trueDecorationType, trueRanges);
-            editor.setDecorations(falseDecorationType, falseRanges);
-        }
-    }
+		}
+	}
 
     function triggerUpdateDecorations() {
         if (timeout) {
@@ -98,6 +112,38 @@ export function activate(context: vscode.ExtensionContext) {
             triggerUpdateDecorations();
         }
     }, null, context.subscriptions);
+}
+
+// Get the JSON File
+function getJSONData(JSONPath: string): any {
+	let fileContents = fs.readFileSync(JSONPath, "utf8");
+	let data: any = JSON.parse(fileContents);
+	return data;
+}
+
+// To get the JSON file path that the extension will be looking
+// This JSON describe what keyword to recolor
+function getJSONPath(language: string|null): string {
+	// if language passed in is null, then set it to 'default.json'
+	if(language == null)
+		language = 'default'
+	return vscode.env.appRoot + '/word-recolor/' + language + '.json';
+}
+
+// To get the current active editor language
+function getCurrentActiveEditorLanguage(): string {
+	let language;
+	let editor = vscode.window.activeTextEditor;
+	
+	if (editor) {
+		language = editor.document.languageId;
+	}
+	else
+	{
+		language = 'No language detected';
+	}
+
+	return language;
 }
 
 // Generated by Bing
